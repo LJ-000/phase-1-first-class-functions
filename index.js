9c4fcd9764cc6b4b790e22ea5168b7f7428855a4
Laura Nielsen <nielsen.j.laura@gmail.com>
2021-04-06 18:06:10 -0400

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,19 @@
+function receivesAFunction(callBack) {
+    callBack ()
+} 
+
+var fn 
+var call = function () {
+return fn 
+}
+function returnsANamedFunction(call) {
+  return function newFunction () {
+
+    }
+}
+
+function returnsAnAnonymousFunction() {
+    return function () {
+        
+    }
+}
